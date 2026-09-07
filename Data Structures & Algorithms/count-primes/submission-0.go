a559bcf283b1838c9de5b0437ee6bd15b6901841
duyan2509 <138445511+duyan2509@users.noreply.github.com>
2026-09-07 22:51:02 +0700

    Add: count-primes - submission-0

---
+++ Data Structures & Algorithms/count-primes/submission-0.go
@@ -0,0 +1,23 @@
+func countPrimes(n int) int {
+	if n<=2{
+		return 0
+	}
+	isPrime:=make([]bool, n)
+	for i:=2;i<n;i++{
+		isPrime[i]=true
+	}
+	for i:=2;i*i<n;i++{
+		if isPrime[i]{
+			for j:=i*i;j<n;j+=i {
+				isPrime[j]=false
+			}
+		}
+	}
+	count := 0
+	for i:=2;i<n;i++ {
+		if isPrime[i] {
+			count++
+		}
+	}
+	return count
+}
